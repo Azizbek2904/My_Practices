9d1972059235744aa7acb06a68fc2ff53ab37745
Azizbek <azizbekqudratov537@gmail.com>
2024-03-27 09:34:43 -0400

    FilesContents

---
+++ src/Base64/Main.java
@@ -0,0 +1,13 @@
+package Base64;
+
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+        String files ="img.png";
+
+        File file = new File(files);
+        byte[] fileContents = new byte[(int) file.length()];
+
+    }
+}
