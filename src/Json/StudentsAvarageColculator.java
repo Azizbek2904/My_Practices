02a38520bba3371569121d6e993e060f03736655
Azizbek <azizbekqudratov537@gmail.com>
2024-03-27 10:14:38 -0400

    file json

--- src/Json/StudentsAvarageColculator.java
+++ src/Json/StudentsAvarageColculator.java
@@ -2,6 +2,7 @@ package Json;
 
 public class StudentsAvarageColculator {
     public static void main(String[] args) {
+     String files="/home/azizbek/IdeaProjects/My_Practices/studentsAvarage";
 
     }
 }
