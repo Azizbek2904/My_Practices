d43cae651b6042d6501d1c3105dcc39fc36d9662
Azizbek <azizbekqudratov537@gmail.com>
2024-03-27 11:24:26 -0400

    gson

--- src/Json/StudentsAvarageColculator.java
+++ src/Json/StudentsAvarageColculator.java
@@ -7,16 +7,20 @@ package Json;
  import java.nio.file.Files;
  import java.nio.file.Path;
  import java.util.List;
+ import java.util.stream.Collectors;
 
 public class StudentsAvarageColculator {
     public static void main(String[] args) throws Exception {
-     String Jsonfiles="/home/azizbek/IdeaProjects/My_Practices/studentsAvarage";
+     String Jsonfiles="/home/azizbek/IdeaProjects/My_Practices/studentsAvarage.json";
      String resultsAvarage= "StudentsAvarage.txt";
 
      String Jsons = Files.readString(Path.of(Jsonfiles));
         Gson gson =new Gson();
-        List<Students> students = gson.fromJson(resultsAvarage, new TypeToken<List<Students>>() {}.getType());
+        List<Students> students = gson.fromJson(Jsons, new TypeToken<List<Students>>() {}.getType());
 
+        double average =students.stream().collect((Collectors.averagingDouble(Students::getAge)));
 
+        String avaragText ="Avarage age of Students-> " +average;
+        Files.writeString(Path.of(resultsAvarage),avaragText);
     }
 }
