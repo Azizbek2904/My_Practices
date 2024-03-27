713bef1559a5bb137355817b4098926efa116c82
Azizbek <azizbekqudratov537@gmail.com>
2024-03-27 10:48:45 -0400

    Students and gson

---
+++ src/Json/Students.java
@@ -0,0 +1,16 @@
+package Json;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Students {
+    private String name;
+    private int age;
+
+}
--- src/Json/StudentsAvarageColculator.java
+++ src/Json/StudentsAvarageColculator.java
@@ -1,8 +1,22 @@
 package Json;
 
+ import com.google.gson.Gson;
+ import com.google.gson.reflect.TypeToken;
+ import org.ietf.jgss.GSSContext;
+
+ import java.nio.file.Files;
+ import java.nio.file.Path;
+ import java.util.List;
+
 public class StudentsAvarageColculator {
-    public static void main(String[] args) {
-     String files="/home/azizbek/IdeaProjects/My_Practices/studentsAvarage";
+    public static void main(String[] args) throws Exception {
+     String Jsonfiles="/home/azizbek/IdeaProjects/My_Practices/studentsAvarage";
+     String resultsAvarage= "StudentsAvarage.txt";
+
+     String Jsons = Files.readString(Path.of(Jsonfiles));
+        Gson gson =new Gson();
+        List<Students> students = gson.fromJson(resultsAvarage, new TypeToken<List<Students>>() {}.getType());
+
 
     }
 }
