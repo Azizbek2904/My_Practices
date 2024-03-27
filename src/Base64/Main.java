45fea1c10111194a7eda05fa45df2a19bfbc9858
Azizbek <azizbekqudratov537@gmail.com>
2024-03-27 09:49:25 -0400

    Base64 full

--- src/Base64/Main.java
+++ src/Base64/Main.java
@@ -1,13 +1,25 @@
 package Base64;
 
-import java.io.File;
+import java.io.*;
+import java.util.Base64;
 
 public class Main {
-    public static void main(String[] args) {
-        String files ="img.png";
+    public static void main(String[] args) throws Exception {
+        String files ="/home/azizbek/IdeaProjects/My_Practices/img.png";
+        String outputFiles ="set.txt";
 
         File file = new File(files);
         byte[] fileContents = new byte[(int) file.length()];
+            FileInputStream inputStream = new FileInputStream(file);
+            inputStream.read(fileContents);
 
+            Base64.Encoder encoder =Base64.getEncoder();
+            String encoderString =encoder.encodeToString(fileContents);
+
+        try (FileWriter writer = new FileWriter(outputFiles)) {
+            writer.write(encoderString);
+        }
+
+        System.out.printf("turi->" + outputFiles);
     }
 }
